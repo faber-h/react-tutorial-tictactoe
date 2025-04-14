feae69e5aee54022e522c0fb7dc70529360363e4
h-faber <hfaberlu@gmail.com>
2025-04-14 15:53:28 +0900

    props를 통해 데이터 전달하기

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-// 컴포넌트 생성
-// 컴포넌트 : 사용자 인터페이스 일부를 표시하는 재사용 가능한 코드 조각
-// 애플리케이션의 UI 엘리먼트를 렌더링, 관리, 업데이트할 때 사용
+// React 컴포넌트 아키텍처 사용 -> 중복된 코드 피하기
+// Board와 Square 컴포넌트는 반드시 대문자로 시작해야 함
+// { value } : props
+// props를 사용하여 부모 컴포넌트에서 자식 컴포넌트로 전달
+// JSX에서 JavaScript로 탈출하려면 중괄호 필요
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
 
-// 함수를 정의
-// export 키워드 : 함수를 파일 외부에서 접근할 수 있도록 만들어줌
-// default 키워드 : 코드를 사용하는 다른 파일에서 이 함수가 파일의 주요 함수임을 알려줌
 export default function Board() {
-  // 버튼을 반환
-  // <button> : JSX 엘리먼트
-  // JSX 엘리먼트 : JavasScript 코드와 HTML 태그의 조합으로 표시할 내용을 설명
-  // className="square" : 버튼 prop 또는 프로퍼티, CSS에 버튼의 스타일을 지정하는 방법 알려줌
-  // X : 버튼 내부에 표시되는 텍스트
-  // </button> : JSX 엘리먼트를 닫아 버튼 내부에 다음 콘텐츠를 배치해서는 안 됨을 나타냄
-
-  // React 컴포넌트는 단일 JSX 엘리먼트를 반환해야 함 -> Fragments(<> </>) 사용하여 여러 개의 JSX 엘리먼트 감싸기
+  // JSX 문법을 사용하여 Square 컴포넌트 렌더링
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
